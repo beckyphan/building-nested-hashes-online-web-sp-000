ca8428d7a2b56fd9c121f7e7940798db4f8d97bf
Becky Phan <beckyphan@live.com>
2019-05-31 02:54:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,24 +7,13 @@ def third_challenge
       :hero => {
         name: "Romero", age: "15", status: "alive"},
       hero_friends: []
-   }, 
+    }, 
    :capulet => {
-      :patriarch => {
-        name: "Lord Capulet",
-        age: "51"
-        },
-      :matriarch => {
-        name: "Lady Capulet",
-        age: "51"
-        },
-      :heroine => {
-        name: "Juliet",
-        age: "15",
-        status: "alive"
-        },
+      :patriarch => {name: "Lord Capulet", age: "51"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
    }
   }
-
   
 end
