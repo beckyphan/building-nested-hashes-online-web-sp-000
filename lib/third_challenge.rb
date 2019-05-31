5a10b53835e25b164134b71904a8bbc9c5e8b03d
Becky Phan <beckyphan@live.com>
2019-05-31 02:53:13 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,18 +2,10 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: "Lord Montague",
-        age: "53"
-        },
-      :matriarch => {
-        name: "Lady Montague"
-        age: "54"
-        },
+        name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {
-        name: "Romero",
-        age: "15",
-        status: "alive"
-        },
+        name: "Romero", age: "15", status: "alive"},
       hero_friends: []
    }, 
    :capulet => {
